e708323f876e6e6bed06268857e4fcf0425f72fb
Vladislav <vladislav9987rod1@mail.ru>
2026-07-06 22:19:03 +0300

    v1.0

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,25 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const apiTarget = {
+  target: 'http://109.68.213.17',
+  changeOrigin: true,
+  cookieDomainRewrite: 'localhost',
+  proxyTimeout: 0,
+  timeout: 0,
+}
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/auth': apiTarget,
+      '/users': apiTarget,
+      '/workspaces': apiTarget,
+      '/tasks': apiTarget,
+      '/comments': apiTarget,
+      '/notifications': apiTarget,
+    },
+  },
 })
